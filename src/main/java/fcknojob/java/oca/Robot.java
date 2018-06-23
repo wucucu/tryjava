5e53c7c9d9bcb59f06ac1cc2ea55742bdff0736e
kc <kc@danamica.dk>
2018-06-23 22:47:58 +0200

    oca Q10

---
+++ src/main/java/fcknojob/java/oca/Robot.java
@@ -0,0 +1,18 @@
+/* OCA_OCP Practice Book by Selikoff and Boyarsky
+ * Question 10
+ */
+package fcknojob.java.oca;
+
+public class Robot {
+    static String weight = "A lot";
+    double ageMonths = 5, ageDays = 2;
+    private static boolean success = true;
+    public void main(String[] args) {
+        final String retries = "1";
+        System.out.print(weight);
+        System.out.print(ageDays);
+        System.out.print(ageMonths);
+        System.out.print(success);
+        System.out.print(retries);
+    }
+}
