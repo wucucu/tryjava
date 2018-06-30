d256f563952f38698caae5a8197a5619e5b16d03
kc <kc@danamica.dk>
2018-06-30 18:29:57 +0200

    leetcode 127 change the way to find 1 differ-char word

--- src/main/java/fcknojob/java/leetcode/_127_word_ladder/Solution.java
+++ src/main/java/fcknojob/java/leetcode/_127_word_ladder/Solution.java
@@ -4,7 +4,9 @@ package fcknojob.java.leetcode._127_word_ladder;
  * BFS
  */
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
 
@@ -34,32 +36,28 @@ class Solution {
 
             Set<String> qNext = new HashSet<>();
             for (String currWord : q) {
-                notVisitedWordSet.remove(q);
-                for(String nextWord: notVisitedWordSet) {
-                    if (isConnected(currWord, nextWord)) {
+                notVisitedWordSet.remove(currWord);
+
+                char[] cs = currWord.toCharArray();
+
+                for (int i = 0; i < cs.length; i++) {
+                    char cTemp = cs[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        cs[i] = c;
+                        String nextWord = new String(cs);
                         if (qBackup.contains(nextWord)) return depth + 1;
-                        qNext.add(nextWord);
+                        if (notVisitedWordSet.contains(nextWord)) qNext.add(nextWord);
                     }
+                    cs[i] = cTemp;
                 }
+
+
             }
             q = qNext;
         }
 
         return 0;
 
-    }
-
-    // assume w1, w2 same length
-    private boolean isConnected(String w1, String w2) {
-        int l = w1.length();
-        boolean isAlreadyOneDiff = false;
-        for (int i = 0; i < l; i++) {
-            if (w1.charAt(i) != w2.charAt(i)) {
-                if (isAlreadyOneDiff) return false;
-                isAlreadyOneDiff = true;
-            }
-        }
-        return isAlreadyOneDiff;
     }
 
 }
