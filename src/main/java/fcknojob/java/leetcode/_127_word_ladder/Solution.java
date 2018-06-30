4c2ec066b1851376c1ab4ed385f8e495db15939b
kc <kc@danamica.dk>
2018-06-30 18:04:04 +0200

    leetcode 127 two-end bfs

--- src/main/java/fcknojob/java/leetcode/_127_word_ladder/Solution.java
+++ src/main/java/fcknojob/java/leetcode/_127_word_ladder/Solution.java
@@ -11,29 +11,38 @@ class Solution {
     // beginword and endword are not same
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        Set<String> notVisitedWordSet = new HashSet<>(wordList);
         wordList.add(beginWord);
+        Set<String> notVisitedWordSet = new HashSet<>(wordList);
 
         if (!notVisitedWordSet.contains(endWord)) return 0;
 
         Set<String> q = new HashSet<>();
+        Set<String> qBackup = new HashSet<>();
         int depth = 0;
         q.add(beginWord);
+        qBackup.add(endWord);
 
-        while (!q.isEmpty()) {
-           depth++;
-           Set<String> qNext = new HashSet<>();
-           for (String currWord: q) {
-               for (Iterator<String> iter = notVisitedWordSet.iterator(); iter.hasNext(); ) {
-                   String nextWord = iter.next();
-                   if (isConnected(currWord, nextWord)) {
-                       if (nextWord.equals(endWord)) return depth+1;
-                       iter.remove();
-                       qNext.add(nextWord);
-                   }
-               }
-           }
-           q = qNext;
+        while (!q.isEmpty() && !qBackup.isEmpty()) {
+
+            depth++;
+
+            if (q.size() > qBackup.size()) {
+                Set<String> qTemp = q;
+                q = qBackup;
+                qBackup = qTemp;
+            }
+
+            Set<String> qNext = new HashSet<>();
+            for (String currWord : q) {
+                notVisitedWordSet.remove(q);
+                for(String nextWord: notVisitedWordSet) {
+                    if (isConnected(currWord, nextWord)) {
+                        if (qBackup.contains(nextWord)) return depth + 1;
+                        qNext.add(nextWord);
+                    }
+                }
+            }
+            q = qNext;
         }
 
         return 0;
