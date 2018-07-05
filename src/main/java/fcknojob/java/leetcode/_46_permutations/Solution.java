1748d9a364f49cf7ecfe86e20129c1054b78ec0b
kc <kc@danamica.dk>
2018-07-05 11:35:20 +0200

    init leetcode 46 permutation

---
+++ src/main/java/fcknojob/java/leetcode/_46_permutations/Solution.java
@@ -0,0 +1,17 @@
+package fcknojob.java.leetcode._46_permutations;
+
+/*
+ * N different elemnts have N! permutations in total.
+ * Try trivial backtracking, which needs 2*N! element swap in position.
+ * Try Heap's Algorithm, which needs N! element swap in position.
+ */
+
+import java.util.List;
+
+class Solution {
+
+    public List<List<Integer>> permutate(int[] nums) {
+
+    }
+
+}
