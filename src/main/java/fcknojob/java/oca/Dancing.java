cc218610522cad590cdf700b86cdd790b800a444
wucucu <wucucu@users.noreply.github.com>
2018-10-01 22:57:12 +0200

    variable name typo

--- src/main/java/fcknojob/java/oca/Dancing.java
+++ src/main/java/fcknojob/java/oca/Dancing.java
@@ -6,9 +6,9 @@ package fcknojob.java.oca;
 public class Dancing {
     public static void main(String[] vars) {
         int leaders = 10 * (2 + 2 / 5);
-        int flowers = leaders * 2;
+        int followers = leaders * 2;
         System.out.println(leaders);
-        System.out.println(flowers);
-        System.out.println(leaders + flowers < 10 ? "Too few" : "Too many");
+        System.out.println(followers);
+        System.out.println(leaders + followers < 10 ? "Too few" : "Too many");
     }
 }
