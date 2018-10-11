826924bf241d8e8aaf3b9bdaed31b775d2330bce
wucucu <wucucu@users.noreply.github.com>
2018-10-11 22:04:21 +0200

    oca

---
+++ src/main/java/fcknojob/java/oca/Bond.java
@@ -0,0 +1,22 @@
+/* OCA_OCP Practice Book by Selikoff and Boyarsky
+ * Chaper 3, Question 20
+ */
+package fcknojob.java.oca;
+
+public class Bond {
+    private static int price =5;
+    public boolean sell() {
+        if (price<10) {
+            price++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] cash) {
+        new Bond().sell();
+        new Bond().sell();
+        new Bond().sell();
+        System.out.print(price);
+    }
+}
